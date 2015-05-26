ed6617f1a66097e369116ffa8c72cf85c582d33a
Saeed <s_a@live.com>
2015-05-27 00:13:20 +0200

    some character movement

---
+++ LuxUnityProject/Assets/Scripts/CharacterMove.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterMove : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+    // Update is called once per frame
+    void Update() {
+        if (Input.GetAxis("Horizontal") < -0.5)
+        {
+            gameObject.transform.Translate(Vector3.left * Time.deltaTime * 2.0f);
+            gameObject.transform.localScale = new Vector3(-1, 1);
+        }
+        else if (Input.GetAxis("Horizontal") > 0.5)
+        {
+            gameObject.transform.Translate(Vector3.right * Time.deltaTime * 2.0f);
+            gameObject.transform.localScale = new Vector3(1,1);
+        }
+    }
+}
