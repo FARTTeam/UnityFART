14b870175fdfbaa1665b1c3deba023942f85e8cc
DoomGuy_DELL <s_a@live.com>
2015-05-27 16:03:45 +0200

    Jump fixed with foot sensor, built in CharacterMove.cs

--- LuxUnityProject/Assets/Scripts/CharacterMove.cs
+++ LuxUnityProject/Assets/Scripts/CharacterMove.cs
@@ -4,9 +4,19 @@ using System.Collections;
 public class CharacterMove : MonoBehaviour {
     public float Speed;
     public float JumpPower;
+    public float FootSensorHeight;
+
+    BoxCollider2D mySensor, footSensor;
+    int jumpEnabled = 0;
 	// Use this for initialization
 	void Start () {
-	
+        mySensor = transform.GetComponent<BoxCollider2D>();
+        Vector2 mySize = mySensor.size;
+        Vector2 footSize = new Vector2(mySize.x, mySize.y * FootSensorHeight);
+        footSensor = gameObject.AddComponent<BoxCollider2D>();
+        footSensor.size = footSize;
+        footSensor.offset = new Vector2(0, mySize.y * -0.5f);
+        footSensor.isTrigger = true;
 	}
 
     // Update is called once per frame
@@ -21,9 +31,19 @@ public class CharacterMove : MonoBehaviour {
             transform.Translate(Vector3.right * Time.deltaTime * Speed);
             transform.localScale = new Vector3(Mathf.Abs(transform.localScale.x),transform.localScale.y);
         }
-        if (Input.GetButton("Jump"))
+        if (Input.GetButtonDown("Jump") && jumpEnabled > 0)
         {
             transform.GetComponent<Rigidbody2D>().AddForce(new Vector2(0, JumpPower), ForceMode2D.Impulse);
         }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (footSensor.IsTouching(other) && other.gameObject.layer == 8) jumpEnabled++;
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (!footSensor.IsTouching(other) && other.gameObject.layer == 8) jumpEnabled--;
     }
 }
