e75fae503e1a2fad15ba8f9bad94f5854f8fe3b8
DoomGuy_DELL <s_a@live.com>
2015-05-27 12:16:29 +0200

    some jump and gravity tweak

--- LuxUnityProject/Assets/Scripts/CharacterMove.cs
+++ LuxUnityProject/Assets/Scripts/CharacterMove.cs
@@ -2,7 +2,8 @@
 using System.Collections;
 
 public class CharacterMove : MonoBehaviour {
-
+    public float Speed;
+    public float JumpPower;
 	// Use this for initialization
 	void Start () {
 	
@@ -12,13 +13,17 @@ public class CharacterMove : MonoBehaviour {
     void Update() {
         if (Input.GetAxis("Horizontal") < -0.5)
         {
-            gameObject.transform.Translate(Vector3.left * Time.deltaTime * 2.0f);
-            gameObject.transform.localScale = new Vector3(-1, 1);
+            transform.Translate(Vector3.left * Time.deltaTime * Speed);
+            transform.localScale = new Vector3(-Mathf.Abs(transform.localScale.x), transform.localScale.y);
         }
         else if (Input.GetAxis("Horizontal") > 0.5)
         {
-            gameObject.transform.Translate(Vector3.right * Time.deltaTime * 2.0f);
-            gameObject.transform.localScale = new Vector3(1,1);
+            transform.Translate(Vector3.right * Time.deltaTime * Speed);
+            transform.localScale = new Vector3(Mathf.Abs(transform.localScale.x),transform.localScale.y);
+        }
+        else if (Input.GetButton("Jump"))
+        {
+            transform.GetComponent<Rigidbody2D>().AddForce(new Vector2(0, JumpPower), ForceMode2D.Impulse);
         }
     }
 }
